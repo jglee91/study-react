3a079cbddd7a75d8dc08ebfdc2111ae32f7d6c9f
jglee <dlwnsrb2010@gmail.com>
2021-09-01 16:00:51 +0900

    tutorial react-with-typescript

---
+++ react-with-typescript/src/App.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import List from './components/List';
+import AddToList from './components/AddToList';
+
+export interface IState {
+  people: {
+    name: string;
+    url: string;
+    age: number;
+    note?: string;
+  }[];
+}
+
+const App = () => {
+  const [people, setPeople] = useState<IState['people']>([
+    {
+      name: 'LeBron James',
+      url: 'https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png',
+      age: 36,
+      note: 'Allegeric to staying on the same team',
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <h1>People Invited to my Party</h1>
+      <List people={people} />
+      <AddToList people={people} setPeople={setPeople} />
+    </div>
+  );
+};
+
+export default App;
