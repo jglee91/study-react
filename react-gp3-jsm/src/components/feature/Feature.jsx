9605d7cf943f2d54c6a91a2e3ee85b311063b318
jglee <dlwnsrb2010@gmail.com>
2022-03-08 00:42:42 +0900

    gpt3-jsm - what is gp3 section ~ features

--- react-gp3-jsm/src/components/feature/Feature.jsx
+++ react-gp3-jsm/src/components/feature/Feature.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './feature.css';
 
-function Feature() {
-  return <div>Feature</div>;
+function Feature({ title, text }) {
+  return (
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container__feature-text">
+        <p>{text}</p>
+      </div>
+    </div>
+  );
 }
+
+Feature.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
 export default Feature;
